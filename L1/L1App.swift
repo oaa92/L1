52a3c912d130d24c36647954a983655ffac73ae5
Anatoliy Odinetskiy <aodinetsky@yandex-team.ru>
2024-03-02 20:31:30 +0700

    Initial Commit

---
+++ L1/L1App.swift
@@ -0,0 +1,17 @@
+//
+//  L1App.swift
+//  L1
+//
+//  Created by Anatoliy Odinetskiy on 02.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct L1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
